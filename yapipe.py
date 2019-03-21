a27fd3ea646a00861aa2ddbeda380d08ec741f9a
Pav_H <phomutov@sfedu.ru>
2019-03-21 13:00:20 +0300

    примитивный прототип #2 с портами в классах, возможностью брать последний результат по нажатию enter

--- yapipe.py
+++ yapipe.py
@@ -2,78 +2,95 @@ import time
 
 
 class Event(object):  # базовый класс
-    pass
+	pass
 
 
 class Sum(Event):  # обрабатывает событие суммы
-    def __init__(self, a, b):  # инициализирует объект суммы
-        self.type = 'SUM'
-        self.operand1 = a
-        self.operand2 = b
+	def __init__(self, a, b):  # инициализирует объект суммы
+		self.type = 'SUM'
+		self.operand1 = a
+		self.operand2 = b
+		self.res = None
 
-    def do(self):  # метод суммы
-        res = int(self.operand1) + int(self.operand2)
-        return res
+	def do(self):  # метод суммы
+		try:
+			self.res = int(self.operand1) + int(self.operand2)
+			return self.res
+		except ValueError:
+			print("Wrong data")
+		except TypeError:
+			print("Wrong Type")
 
 
 class Mul(Event):  # обрабатывает событие умножения
-    def __init__(self, a, b):  # инициализирует объект умножения
-        self.type = 'MUL'
-        self.operand1 = a
-        self.operand2 = b
+	def __init__(self, a, b):  # инициализирует объект умножения
+		self.type = 'MUL'
+		self.operand1 = a
+		self.operand2 = b
+		self.res = None
 
-    def do(self):  # метод умножения
-        res = int(self.operand1) * int(self.operand2)
-        return res
+	def do(self):  # метод суммы
+		try:
+			self.res = int(self.operand1) + int(self.operand2)
+			return self.res
+		except ValueError:
+			print("Wrong data")
+		except TypeError:
+			print("Wrong Type")
 
 
 class Concat(Event):  # обрабатывает событие конкатениции
-    def __init__(self, a, b):  # инициализирует объект конкатениции
-        self.type = 'CONCAT'
-        self.operand1 = a
-        self.operand2 = b
+	def __init__(self, a, b):  # инициализирует объект конкатениции
+		self.type = 'CONCAT'
+		self.operand1 = a
+		self.operand2 = b
+		self.res = None
 
-    def do(self):  # метод конкатенации
-        res = self.operand1 + self.operand2
-        return res
+	def do(self):  # метод суммы
+		try:
+			self.res = str(self.operand1) + self.operand2
+			return self.res
+		except ValueError:
+			print("Wrong Value")
+		except TypeError:
+			print("Wrong Type")
 
 
-# operation_list = {'+': Sum(), '*': Mul(), 'CONCAT': Concat()}
 operation_list = ['+', '*', 'CONCAT']
 
-"""
-q1 = queue.Queue()
-q2 = queue.Queue()
-"""
-
+obj = None
 while True:  # петля
-    print("input A")
-    A = input()
-    if A == 'end':
-        break
-    print("input B")
-    B = input()
-    print("input Operation")
-    meta_operation = input()  # данные для классификации события
+	f = True
+	while f:
+		A = input("input A or press ↲ for result in previous event: ")
+		if (obj is not None) & (A == ''):
+				A = obj.res
+				print("input A: ", A)
+		if A != '':
+			f = False
+	if A == 'end':
+		break
+	B = input("input B: ")
 
-    # if meta_operation in operation_list.keys():
-    # work = operation_list(meta_operation)
+	meta_operation = input("input Operation: ")  # данные для классификации события
 
-    """ !!! РЕАЛИЗОВАТЬ СЛОВАРЬ !!!  """
-    if meta_operation in operation_list:
-        if meta_operation == '+':
-            obj = Sum(A, B)
-            print("Sum = ", obj.do())
+	if meta_operation in operation_list:  # соотнесение ввода к событию
+		if meta_operation == '+':
+			obj = Sum(A, B)
+			print("Sum = ", obj.do())
 
-        elif meta_operation == '*':
-            obj = Mul(A, B)
-            print("Mul = ", obj.do())
+		elif meta_operation == '*':
+			obj = Mul(A, B)
+			print("Mul = ", obj.do())
 
-        elif meta_operation == 'CONCAT':
-            obj = Concat(A, B)
-            print("Concat = ", obj.do())
-        else:
-            print("UNKNOWN OPERATION")
-    print("********")
-    time.sleep(2)  # следующая итерация
+		elif meta_operation == 'CONCAT':
+			obj = Concat(A, B)
+			print("Concat = ", obj.do())
+	else:
+		print("UNKNOWN OPERATION")
+
+for j in range(8):  # следующая итерация
+	print('*', end=' ')
+	time.sleep(0.2)
+
 print("Exiting loop")
