35265fd2cdafef66276fa17d3fb0e47935cc7d12
Pav_H <phomutov@sfedu.ru>
2019-04-25 13:52:30 +0300

    примитивный прототип #3 Модульные тесты и прототип графа

--- yapipe.py
+++ yapipe.py
@@ -1,4 +1,5 @@
 import time
+import unittest
 
 
 class Event(object):  # базовый класс
@@ -31,7 +32,7 @@ class Mul(Event):  # обрабатывает событие умножения
 
 	def do(self):  # метод суммы
 		try:
-			self.res = int(self.operand1) + int(self.operand2)
+			self.res = int(self.operand1) * int(self.operand2)
 			return self.res
 		except ValueError:
 			print("Wrong data")
@@ -48,7 +49,7 @@ class Concat(Event):  # обрабатывает событие конкатен
 
 	def do(self):  # метод суммы
 		try:
-			self.res = str(self.operand1) + self.operand2
+			self.res = str(self.operand1) + str(self.operand2)
 			return self.res
 		except ValueError:
 			print("Wrong Value")
@@ -56,41 +57,107 @@ class Concat(Event):  # обрабатывает событие конкатен
 			print("Wrong Type")
 
 
-operation_list = ['+', '*', 'CONCAT']
+class YapipeTest(unittest.TestCase):
+	"""yapipe tests"""
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Tear down for class"""
+		print("==========")
+		print("tearDownClass")
+
+	def setUp(self):
+		"""Set up for test"""
+		print("Set up for [" + self.shortDescription() + "]")
+
+	def tearDown(self):
+		"""Tear down for test"""
+		print("Tear down for [" + self.shortDescription() + "]")
+		print("")
+
+	def test_sum(self):
+		"""Sum test"""
+		print("id: " + self.id())
+		testobj = Sum(1, 2)
+		testobj.do()
+		self.assertEqual(3, testobj.res)
+
+	def test_mul(self):
+		"""Mul test"""
+		print("id: " + self.id())
+		testobj = Mul(1, 3)
+		testobj.do()
+		self.assertEqual(3, testobj.res)
+
+	def test_concat(self):
+		"""Concat test"""
+		print("id: " + self.id())
+		testobj = Concat('ya', 'pipe')
+		testobj.do()
+		self.assertEqual('yapipe', testobj.res)
+
+
+# список с метками операций
+operation_list = ['+', '*', 'CONCAT', 'concat', 'Concat','CON', 'Con', 'con']
+
+# запуск тестов
+if __name__ == '__main__':
+	unittest.main(exit=False)
+	time.sleep(0.1)
+print("")
+
 
 obj = None
+A = ''
 while True:  # петля
 	f = True
+	operation_set = []
+	# Первый ввод начальных данных
 	while f:
-		A = input("input A or press ↲ for result in previous event: ")
-		if (obj is not None) & (A == ''):
-				A = obj.res
-				print("input A: ", A)
+		A = input("input A or press ↲ for result in last event ('end' to stop): ")
+		if (obj is not None) and (A == ''):
+			A = obj.res
+			print("input A: ", A)
 		if A != '':
 			f = False
 	if A == 'end':
 		break
 	B = input("input B: ")
 
-	meta_operation = input("input Operation: ")  # данные для классификации события
+	# ввод списка дуг через перечисление операций
+	print("Input list of operations(input ' ' to stop input)")
+	c = True
+	while c:
+		c = input()
+		if c:
+			operation_set.append(c)
+	print(operation_set)
 
-	if meta_operation in operation_list:  # соотнесение ввода к событию
-		if meta_operation == '+':
-			obj = Sum(A, B)
-			print("Sum = ", obj.do())
+	# данные для классификации события
+	for i in range(0, len(operation_set)):
+		meta_operation = operation_set[i]
+		if meta_operation in operation_list:  # соотнесение ввода к событию
+			if i != 0:
+				print("Input operand for next operation")
+				A = input()
+			if meta_operation == '+':  # Sum
+				obj = Sum(A, B)
+				print("Sum = ", obj.do())
+				B = obj.res
+			elif meta_operation == '*':  # Mul
+				obj = Mul(A, B)
+				print("Mul = ", obj.do())
+				B = obj.res
+			elif meta_operation == 'CONCAT' or 'concat' or 'Concat' or 'CON' or 'Con' or 'con':  # Concat
+				obj = Concat(A, B)
+				print("Concat = ", obj.do())
+				B = obj.res
+		else:
+			print("UNKNOWN OPERATION")
 
-		elif meta_operation == '*':
-			obj = Mul(A, B)
-			print("Mul = ", obj.do())
-
-		elif meta_operation == 'CONCAT':
-			obj = Concat(A, B)
-			print("Concat = ", obj.do())
-	else:
-		print("UNKNOWN OPERATION")
-
-for j in range(8):  # следующая итерация
-	print('*', end=' ', flush=True)
-	time.sleep(0.2)
-
-print("Exiting loop")
+	for j in range(8):  # следующая итерация
+		print('*', end=' ', flush=True)
+		time.sleep(0.1)
+	print("")
+	print("Exiting loop")
+	print("")
