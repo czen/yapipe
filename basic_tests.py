386b551bd69adeb1b7f281990043fb9e9f59fa77
Pavel Khomutov <p.khomutov@gmail.com>
2020-03-10 20:33:19 +0300

    Починил тесты

--- basic_tests.py
+++ basic_tests.py
@@ -34,10 +34,9 @@ class YapipeTest(unittest.TestCase):
         testobj = Sum()
         testobj2 = Sum()
         testobj2._add_port('R')
+        testobj.link(testobj2, 'R')
         testobj.send_data('term1', 1)
         testobj.send_data('term2', 2)
-        testobj.link(testobj2, 'R')
-        testobj.do('term1', 'term2')
         self.assertEqual(3, testobj2.get_data('R'))
 
     def test_mul(self):
@@ -46,10 +45,9 @@ class YapipeTest(unittest.TestCase):
         testobj = Mul()
         testobj2 = Mul()
         testobj2._add_port('R')
+        testobj.link(testobj2, 'R')
         testobj.send_data('multiplier1', 2)
         testobj.send_data('multiplier2', 3)
-        testobj.link(testobj2, 'R')
-        testobj.do('multiplier1', 'multiplier2')
         self.assertEqual(6, testobj2.get_data('R'))
 
     def test_concat(self):
@@ -58,10 +56,9 @@ class YapipeTest(unittest.TestCase):
         testobj = Concat()
         testobj2 = Concat()
         testobj2._add_port('R')
+        testobj.link(testobj2, 'R')
         testobj.send_data('string1', "ya")
         testobj.send_data('string2', "pipe")
-        testobj.link(testobj2, 'R')
-        testobj.do('string1', 'string2')
         self.assertEqual("yapipe", testobj2.get_data('R'))
 
 
